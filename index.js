3bc93cf659625f99ed007e5c8f27f58e8a003f71
TheyKay <murraykyleb@gmail.com>
2021-03-08 21:07:29 -0700

    moved index.html to root directory so it deploys on github pages

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'))
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 app.get("/about", (req, res) => {
